1cf165800898827444c71b0e78c5f03565844869
HazkerMax <hazker.pop@gmail.com>
2022-11-06 21:32:56 +0300

    init new projects

---
+++ TodoList/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var a = 150;
+        Console.WriteLine("Созидать " + a);
+        Console.WriteLine("Созидать " + a);
+        Console.WriteLine("Созидать " + a);
+        Console.WriteLine("Созидать " + a);
+        Console.WriteLine("Созидать " + a);
+        Console.WriteLine("Созидать " + a);
+        Console.WriteLine("Созидать " + a);
+        Console.WriteLine("Созидать " + a);
+        Console.WriteLine("Созидать " + a);
+    }
+}
